e7178d084369b9a6d9c9165af899feb3c93fa19d
Ilias Jennane <iliasj@microsoft.com>
2020-08-24 09:21:02 -0400

    limited the api return to 10 items

--- simpleapp/simpleapi/Controllers/WeatherForecastController.cs
+++ simpleapp/simpleapi/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace simpleapi.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(0, 15).Select(index => new WeatherForecast
+            return Enumerable.Range(0, 10).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
